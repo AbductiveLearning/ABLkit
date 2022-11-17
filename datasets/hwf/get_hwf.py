22de038d343487e81600fd2ba1a36c722590e6f6
troyyyyy <49091847+troyyyyy@users.noreply.github.com>
2022-11-17 10:26:15 +0800

    Update get_hwf.py

--- datasets/hwf/get_hwf.py
+++ datasets/hwf/get_hwf.py
@@ -7,10 +7,10 @@ img_transform = transforms.Compose([
                         transforms.Normalize((0.5,), (1,))
                     ])
 
-def get_data(file):
+def get_data(file, precision_num = 2):
     X = []
     Y = []
-    img_dir = './data/Handwritten_Math_Symbols/'
+    img_dir = './datasets/hwf/data/Handwritten_Math_Symbols/'
     with open(file) as f:
         data = json.load(f)
         for idx in range(len(data)):
@@ -20,12 +20,12 @@ def get_data(file):
                 img = img_transform(img)
                 imgs.append(img)
             X.append(imgs)
-            Y.append(data[idx]['res'])
+            Y.append(round(data[idx]['res'], precision_num))
     return X, Y
 
-def get_hwf():
-    train_X, train_Y = get_data('./data/expr_train.json')
-    test_X, test_Y = get_data('./data/expr_test.json')
+def get_hwf(precision_num = 2):
+    train_X, train_Y = get_data('./datasets/hwf/data/expr_train.json', precision_num)
+    test_X, test_Y = get_data('./datasets/hwf/data/expr_test.json', precision_num)
     
     return train_X, train_Y, test_X, test_Y
 
