4677d2651f9b0c589a42b7caa34b3ce19fb84185
troyyyyy <49091847+troyyyyy@users.noreply.github.com>
2022-11-22 18:50:22 +0800

    Update framework.py

--- framework.py
+++ framework.py
@@ -16,7 +16,6 @@ import numpy as np
 
 from utils.plog import INFO, DEBUG, clocker
 
-@clocker
 def block_sample(X, Z, Y, sample_num, epoch_idx):
     part_num = (len(X) // sample_num)
     if part_num == 0:
@@ -34,96 +33,40 @@ def get_taglist(self, Z):
     tmp = sorted(list(set(tmp)))
     return tmp
 
-def get_abl_acc(Y, pseudo_Z, logic_forward):
+def get_abl_acc(pred_Z, Y, logic_forward):
     abl_acc = 0
-    for y, pseudo_z in zip(Y, pseudo_Z):
-        if(logic_forward(pseudo_z) == y):
+    for pred_z, y in zip(pred_Z, Y):
+        if(logic_forward(pred_z) == y):
             abl_acc += 1      
     return abl_acc / len(Y)
 
-def get_char_acc(Z, pseudo_Z):
+def get_char_acc(Z, pred_Z):
     char_acc = 0
     char_num = 0
-    for z, pseudo_z in zip(Z, pseudo_Z):
+    for pred_z, z in zip(pred_Z, Z):
         char_num += len(z)
         for zidx in range(len(z)):
-            if(z[zidx] == pseudo_z[zidx]):
+            if(pred_z[zidx] == z[zidx]):
                 char_acc += 1
     return char_acc / char_num
     
-    
-# def result_statistics(pseudo_Y, Y, abduced_Y):
 
-#     abd_err_num = 0
-#     abd_char_num = 0
-#     abd_char_acc = 0
-#     abd_failed = 0
-#     word_err_num = 0
-    
-#     ori_char_num = 0
-#     ori_char_acc = 0
-
-#     for tidx, (pseudo_y, y, abduced_y) in enumerate(zip(pseudo_Y, Y, abduced_Y)):
-#         pseudo_y = pseudo_y
-#         if sum(abduced_y != y) != 0:
-#             abd_err_num += 1
-#         if abduced_y is not None:
-#             abd_char_num += len(y)
-#             abd_char_acc += sum(abduced_y == y)
-#         else:
-#             abd_failed += 1
-
-#         ori_char_num += len(pseudo_y)
-#         ori_char_acc += sum(pseudo_y == y)
-        
-#         if abduced_y is not None and sum(y != pseudo_y) == 0 and sum(pseudo_y != abduced_y) > 0:
-#             INFO(pseudo_y, y, abduced_y)
-#             pk.dump((pseudo_y, y, abduced_y), open("bug.pk", "wb"))
-
-#         if sum(pseudo_y != y) != 0:
-#             word_err_num += 1
-
-#     INFO("")
-#     INFO("Abd word level accuracy:", 1 - word_err_num / len(pseudo_Y))
-#     INFO("Abd char level accuracy:", abd_char_acc / abd_char_num)
-#     INFO("Ori char level accuracy:", ori_char_acc / ori_char_num)
-#     INFO("")
-
-#     result = {"total_word" : len(pseudo_Y), "accuracy_word" : len(pseudo_Y) - word_err_num,
-#               "total_abd_char": abd_char_num, "accuracy_abd_char" : abd_char_acc,
-#               "total_ori_char": ori_char_num, "accuracy_ori_char" : ori_char_acc,
-#               "total_abd_failed": abd_failed}
-
-#     return result
-
-@clocker
 def filter_data(X, abduced_Z):
     finetune_Z = []
     finetune_X = []
     for abduced_x, abduced_z in zip(X, abduced_Z):
-        if abduced_z is not None:
+        if abduced_z is not []:
             finetune_X.append(abduced_x)
             finetune_Z.append(abduced_z)
     return finetune_X, finetune_Z
 
-@clocker
-def is_all_sublabel_exist(labels, std_label_list):
-    if not labels:
-        return False
-    
-    labels = np.array(labels).T
-    for idx, (std_label, label) in enumerate(zip(std_label_list, labels)):
-        std_num = len(set(std_label))
-        sublabel_num = len(set(label))
-        if std_num != sublabel_num:
-            INFO(f"sublabel {idx} should have {std_num} class, but data only have {sublabel_num} class", screen=True)
-            return False
-    return True
-
 def pretrain(model, X, Z):
     pass
 
-def train(model, abducer, X, Z, Y, epochs = 5, sample_num = -1, verbose = -1):
+def train(model, abducer, train_data, test_data, epochs = 5, sample_num = -1, verbose = -1):
+    X, Z, Y = train_data
+    test_X, test_Z, test_Y = test_data
+    
     # Set default parameters
     if sample_num == -1:
         sample_num = len(X)
@@ -146,10 +89,10 @@ def train(model, abducer, X, Z, Y, epochs = 5, sample_num = -1, verbose = -1):
         preds_res = predict_func(X)
         abduced_Z = abduce_func(preds_res, Y)
 
-        abl_acc = get_abl_acc(Y, preds_res['cls'], abducer.kb.logic_forward)
+        abl_acc = get_abl_acc(preds_res['cls'], Y, abducer.kb.logic_forward)
         if(char_acc_flag):
-            ori_char_acc = get_char_acc(Z, preds_res['cls'])
-            abd_char_acc = get_char_acc(abduced_Z, preds_res['cls'])
+            ori_char_acc = get_char_acc(preds_res['cls'], Z)
+            abd_char_acc = get_char_acc(preds_res['cls'], abduced_Z)
             INFO('epoch_idx:', epoch_idx, '  abl_acc:', abl_acc, '  ori_char_acc:', ori_char_acc, '  abd_char_acc:', abd_char_acc)
         else:
             INFO('epoch_idx:', epoch_idx, '  abl_acc:', abl_acc)
@@ -162,50 +105,6 @@ def train(model, abducer, X, Z, Y, epochs = 5, sample_num = -1, verbose = -1):
             INFO("lack of data, all abduced failed", len(finetune_X))
             
     return abl_acc
-
-# def train(model, abducer, X, Y, C = None, epochs = 10, sample_num = -1, verbose = -1, check_sublabel = True):
-#     # Set default parameters
-#     if sample_num == -1:
-#         sample_num = len(X)
-
-#     if verbose < 1:
-#         verbose = epochs
-
-#     if C is None:
-#         C = [None] * len(X)
-
-#     # Set function running time recorder
-#     valid_func = clocker(model.valid)
-#     predict_func = clocker(model.predict)
-#     train_func = clocker(model.train)
-
-#     abduce_func = clocker(abducer.batch_abduce)
-
-#     X_bak = X
-#     Y_bak = Y
-#     C_bak = C
-
-#     # Abductive learning train process
-#     res = {}
-#     for epoch_idx in range(epochs):
-#         X, Y, C = block_sample(X_bak, Y_bak, C_bak, sample_num, epoch_idx)
-#         preds_res = predict_func(X)
-#         abduced_Y = abduce_func(preds_res, C)
-#         finetune_X, finetune_Y = filter_data(X, abduced_Y)
-#         score, score_list = valid_func(X, Y)
-#         if ((epoch_idx + 1) % verbose == 0) or (epoch_idx == epochs - 1):
-#             res = result_statistics(preds_res["cls"], Y, abduced_Y)
-#             INFO(res)
-
-#         if check_sublabel and (not is_all_sublabel_exist(finetune_Y, model.label_lists)):
-#             INFO("There is some sub label missing", len(finetune_Y))
-#             break
-
-#         if len(finetune_X) > 0:
-#             train_func(finetune_X, finetune_Y)#, n_epoch = 10)
-#         else:
-#             INFO("lack of data, all abduced failed", len(finetune_X))
-#     return res
 
 if __name__ == "__main__":
     pass
