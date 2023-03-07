f65a277e4d2f3faef35b5d805898e8a1bb579dd0
Gao Enhao <gaoeh@lamda.nju.edu.cn>
2023-03-07 09:31:46 +0800

    complete knn model for HED

--- example.py
+++ example.py
@@ -11,12 +11,14 @@
 # ================================================================#
 
 from utils.plog import logger, INFO
+from utils.utils import reduce_dimension
 import torch.nn as nn
 import torch
 
 from models.nn import LeNet5, SymbolNet
 from models.basic_model import BasicModel, BasicDataset
 from models.wabl_models import DecisionTree, WABLBasicModel
+from sklearn.neighbors import KNeighborsClassifier
 
 from multiprocessing import Pool
 from abducer.abducer_base import AbducerBase
@@ -25,6 +27,7 @@ from datasets.mnist_add.get_mnist_add import get_mnist_add
 from datasets.hwf.get_hwf import get_hwf
 from datasets.hed.get_hed import get_hed, split_equation
 import framework_hed
+import framework_hed_knn
 
 
 def run_test():
@@ -41,29 +44,45 @@ def run_test():
     total_train_data = get_hed(train=True)
     train_data, val_data = split_equation(total_train_data, 3, 1)
     test_data = get_hed(train=False)
-    
-    # cls = LeNet5(num_classes=len(kb.pseudo_label_list), image_size=(train_data[0][0][0].shape[1:]))
-    cls = SymbolNet(num_classes=len(kb.pseudo_label_list))
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    
-    framework_hed.hed_pretrain(kb, cls, recorder)
-    
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.RMSprop(cls.parameters(), lr=0.001, weight_decay=1e-6)
-    # optimizer = torch.optim.Adam(cls.parameters(), lr=0.00001, betas=(0.9, 0.99))
-    
 
-    base_model = BasicModel(cls, criterion, optimizer, device, save_interval=1, save_dir=recorder.save_dir, batch_size=32, num_epochs=10, recorder=recorder)
+    # ======================== non-NN model ========================== #
+    reduce_dimension(train_data)
+    reduce_dimension(val_data)
+    reduce_dimension(test_data)
+    base_model = KNeighborsClassifier(n_neighbors=3)
+    pretrain_data_X, pretrain_data_Y = framework_hed_knn.hed_pretrain(base_model)
     model = WABLBasicModel(base_model, kb.pseudo_label_list)
-    
-    # train_X, train_Z, train_Y = get_mnist_add(train = True, get_pseudo_label = True)
-    # test_X, test_Z, test_Y = get_mnist_add(train = False, get_pseudo_label = True)
+    model, mapping = framework_hed_knn.train_with_rule(
+        model, abducer, train_data, val_data, (pretrain_data_X, pretrain_data_Y), select_num=10, min_len=5, max_len=8
+    )
+    framework_hed_knn.hed_test(
+        model, abducer, mapping, train_data, test_data, min_len=5, max_len=8
+    )
+    # ============================ End =============================== #
 
-    # train_data = get_hwf(train = True, get_pseudo_label = True)
-    # test_data = get_hwf(train = False, get_pseudo_label = True)
+    # ========================== NN model ============================ #
+    # # cls = LeNet5(num_classes=len(kb.pseudo_label_list), image_size=(train_data[0][0][0].shape[1:]))
+    # cls = SymbolNet(num_classes=len(kb.pseudo_label_list))
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    model, mapping = framework_hed.train_with_rule(model, abducer, train_data, val_data, select_num=10, min_len=5, max_len=8)
-    framework_hed.hed_test(model, abducer, mapping, train_data, test_data, min_len=5, max_len=8)
+    # framework_hed.hed_pretrain(kb, cls, recorder)
+
+    # criterion = nn.CrossEntropyLoss()
+    # optimizer = torch.optim.RMSprop(cls.parameters(), lr=0.001, weight_decay=1e-6)
+    # # optimizer = torch.optim.Adam(cls.parameters(), lr=0.00001, betas=(0.9, 0.99))
+
+    # base_model = BasicModel(cls, criterion, optimizer, device, save_interval=1, save_dir=recorder.save_dir, batch_size=32, num_epochs=10, recorder=recorder)
+    # model = WABLBasicModel(base_model, kb.pseudo_label_list)
+
+    # # train_X, train_Z, train_Y = get_mnist_add(train = True, get_pseudo_label = True)
+    # # test_X, test_Z, test_Y = get_mnist_add(train = False, get_pseudo_label = True)
+
+    # # train_data = get_hwf(train = True, get_pseudo_label = True)
+    # # test_data = get_hwf(train = False, get_pseudo_label = True)
+
+    # model, mapping = framework_hed.train_with_rule(model, abducer, train_data, val_data, select_num=10, min_len=5, max_len=8)
+    # framework_hed.hed_test(model, abducer, mapping, train_data, test_data, min_len=5, max_len=8)
+    # ============================ End =============================== #
 
     recorder.dump()
     return True
--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,18 @@
+import torch
+import torch.nn as nn
 import numpy as np
 from utils.plog import INFO
 from collections import OrderedDict
 
 # for multiple predictions, modify from `learn_add.py`
 def flatten(l):
-    return [item for sublist in l for item in flatten(sublist)] if isinstance(l, list) else [l]
-    
+    return (
+        [item for sublist in l for item in flatten(sublist)]
+        if isinstance(l, list)
+        else [l]
+    )
+
+
 # for multiple predictions, modify from `learn_add.py`
 def reform_idx(flatten_pred_res, save_pred_res):
     re = []
@@ -20,10 +27,12 @@ def reform_idx(flatten_pred_res, save_pred_res):
         i = i + j
     return re
 
+
 def hamming_dist(A, B):
     B = np.array(B)
-    A = np.expand_dims(A, axis = 0).repeat(axis=0, repeats=(len(B)))
-    return np.sum(A != B, axis = 1)
+    A = np.expand_dims(A, axis=0).repeat(axis=0, repeats=(len(B)))
+    return np.sum(A != B, axis=1)
+
 
 def confidence_dist(A, B):
     B = np.array(B)
@@ -31,10 +40,10 @@ def confidence_dist(A, B):
     A = np.expand_dims(A, axis=0)
     A = A.repeat(axis=0, repeats=(len(B)))
     rows = np.array(range(len(B)))
-    rows = np.expand_dims(rows, axis = 1).repeat(axis = 1, repeats = len(B[0]))
+    rows = np.expand_dims(rows, axis=1).repeat(axis=1, repeats=len(B[0]))
     cols = np.array(range(len(B[0])))
-    cols = np.expand_dims(cols, axis = 0).repeat(axis = 0, repeats = len(B))
-    return 1 - np.prod(A[rows, cols, B], axis = 1)
+    cols = np.expand_dims(cols, axis=0).repeat(axis=0, repeats=len(B))
+    return 1 - np.prod(A[rows, cols, B], axis=1)
 
 
 def block_sample(X, Z, Y, sample_num, epoch_idx):
@@ -51,21 +60,24 @@ def block_sample(X, Z, Y, sample_num, epoch_idx):
 
 
 def gen_mappings(chars, symbs):
-	n_char = len(chars)
-	n_symbs = len(symbs)
-	if n_char != n_symbs:
-		print('Characters and symbols size dosen\'t match.')
-		return
-	from itertools import permutations
-	mappings = []
-	# returned mappings
-	perms = permutations(symbs)
-	for p in perms:
-		mappings.append(dict(zip(chars, list(p))))
-	return mappings
+    n_char = len(chars)
+    n_symbs = len(symbs)
+    if n_char != n_symbs:
+        print("Characters and symbols size dosen't match.")
+        return
+    from itertools import permutations
+
+    mappings = []
+    # returned mappings
+    perms = permutations(symbs)
+    for p in perms:
+        mappings.append(dict(zip(chars, list(p))))
+    return mappings
+
 
 def mapping_res(original_pred_res, m):
     return [[m[symbol] for symbol in formula] for formula in original_pred_res]
+
 
 def remapping_res(pred_res, m):
     remapping = {}
@@ -73,10 +85,11 @@ def remapping_res(pred_res, m):
         remapping[value] = key
     return [[remapping[symbol] for symbol in formula] for formula in pred_res]
 
+
 def check_equal(a, b):
     if isinstance(a, (int, float)) and isinstance(b, (int, float)):
         return abs(a - b) <= 1e-3
-    
+
     if isinstance(a, list) and isinstance(b, list):
         if len(a) != len(b):
             return False
@@ -84,6 +97,26 @@ def check_equal(a, b):
             if not check_equal(a[i], b[i]):
                 return False
         return True
-    
-    else:    
-        return a == b       
+
+    else:
+        return a == b
+
+
+def extract_feature(img):
+    extractor = nn.AvgPool2d(2, stride=2)
+    feature_map = np.array(extractor(torch.Tensor(img)))
+    return feature_map.reshape((-1,))
+    return np.concatenate(
+        (np.squeeze(np.sum(img, axis=1)), np.squeeze(np.sum(img, axis=2))), axis=0
+    )
+
+
+def reduce_dimension(data):
+    for truth_value in [0, 1]:
+        for equation_len in range(5, 27):
+            equations = data[truth_value][equation_len]
+            reduced_equations = [
+                [extract_feature(symbol_img) for symbol_img in equation]
+                for equation in equations
+            ]
+            data[truth_value][equation_len] = reduced_equations
