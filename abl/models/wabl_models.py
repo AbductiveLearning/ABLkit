8b3645445b07cc92b801773a5551502f83686a88
Gao Enhao <gaoeh@lamda.nju.edu.cn>
2023-03-30 16:43:43 +0800

    [ENH] Add docstring for WABLBasicModel

--- abl/models/wabl_models.py
+++ abl/models/wabl_models.py
@@ -10,6 +10,7 @@
 #
 # ================================================================#
 from itertools import chain
+from typing import List, Any
 
 
 def get_part_data(X, i):
@@ -33,7 +34,36 @@ def reshape_data(Y, marks):
 
 
 class WABLBasicModel:
-    def __init__(self, base_model, pseudo_label_list):
+    """A basic model for weakly augmented bag learning.
+
+    Parameters
+    ----------
+    base_model : Machine Learning Model
+        The base model to use for training and prediction.
+    pseudo_label_list : List[Any]
+        A list of pseudo labels to use for training.
+
+    Attributes
+    ----------
+    cls_list : List[Any]
+        A list of classifiers.
+    pseudo_label_list : List[Any]
+        A list of pseudo labels to use for training.
+    mapping : dict
+        A dictionary mapping pseudo labels to integers.
+    remapping : dict
+        A dictionary mapping integers to pseudo labels.
+
+    Methods
+    -------
+    predict(X: List[List[Any]]) -> dict
+        Predicts the class labels and probabilities for the given data.
+    valid(X: List[List[Any]], Y: List[Any]) -> float
+        Calculates the accuracy score for the given data.
+    train(X: List[List[Any]], Y: List[Any])
+        Trains the model on the given data.
+    """
+    def __init__(self, base_model, pseudo_label_list: List[Any]):
         self.cls_list = []
         self.cls_list.append(base_model)
 
@@ -43,7 +73,19 @@ class WABLBasicModel:
             zip(list(range(len(pseudo_label_list))), pseudo_label_list)
         )
 
-    def predict(self, X):
+    def predict(self, X: List[List[Any]]) -> dict:
+        """Predicts the class labels and probabilities for the given data.
+
+        Parameters
+        ----------
+        X : List[List[Any]]
+            The data to predict on.
+
+        Returns
+        -------
+        dict
+            A dictionary containing the predicted class labels and probabilities.
+        """
         data_X, marks = merge_data(X)
         prob = self.cls_list[0].predict_proba(X=data_X)
         _cls = prob.argmax(axis=1)
@@ -54,15 +96,37 @@ class WABLBasicModel:
 
         return {"cls": cls, "prob": prob}
 
-    def valid(self, X, Y):
+    def valid(self, X: List[List[Any]], Y: List[Any]) -> float:
+        """Calculates the accuracy score for the given data.
+
+        Parameters
+        ----------
+        X : List[List[Any]]
+            The data to calculate the accuracy score on.
+        Y : List[Any]
+            The true class labels for the given data.
+
+        Returns
+        -------
+        float
+            The accuracy score for the given data.
+        """
         data_X, _ = merge_data(X)
         _data_Y, _ = merge_data(Y)
         data_Y = list(map(lambda y: self.mapping[y], _data_Y))
         score = self.cls_list[0].score(X=data_X, y=data_Y)
-        return score, [score]
+        return score
 
-    def train(self, X, Y):
-        # self.label_lists = []
+    def train(self, X: List[List[Any]], Y: List[Any]):
+        """Trains the model on the given data.
+
+        Parameters
+        ----------
+        X : List[List[Any]]
+            The data to train on.
+        Y : List[Any]
+            The true class labels for the given data.
+        """
         data_X, _ = merge_data(X)
         _data_Y, _ = merge_data(Y)
         data_Y = list(map(lambda y: self.mapping[y], _data_Y))
