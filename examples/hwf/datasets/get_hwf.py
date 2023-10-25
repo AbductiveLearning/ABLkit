3e74ed8e92ee51423e31a97133f6852e75138f81
Gao Enhao <gaoeh@lamda.nju.edu.cn>
2023-10-25 19:43:11 +0800

    [MNT] reformat code and change the path of file

--- examples/hwf/datasets/get_hwf.py
+++ examples/hwf/datasets/get_hwf.py
@@ -1,50 +1,47 @@
+import os
 import json
+
 from PIL import Image
 from torchvision.transforms import transforms
 
-img_transform = transforms.Compose([
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.5,), (1,))
-                    ])
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+img_transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5,), (1,))]
+)
+
 
 def get_data(file, get_pseudo_label):
-    X = []
+    X, Y = [], []
     if get_pseudo_label:
         Z = []
-    Y = []
-    img_dir = './datasets/data/Handwritten_Math_Symbols/'
+    img_dir = os.path.join(CURRENT_DIR, "data/Handwritten_Math_Symbols/")
     with open(file) as f:
         data = json.load(f)
         for idx in range(len(data)):
             imgs = []
             imgs_pseudo_label = []
-            for img_path in data[idx]['img_paths']:
-                img = Image.open(img_dir + img_path).convert('L')
+            for img_path in data[idx]["img_paths"]:
+                img = Image.open(img_dir + img_path).convert("L")
                 img = img_transform(img)
                 imgs.append(img)
                 if get_pseudo_label:
-                    imgs_pseudo_label.append(img_path.split('/')[0])
+                    imgs_pseudo_label.append(img_path.split("/")[0])
             X.append(imgs)
             if get_pseudo_label:
                 Z.append(imgs_pseudo_label)
-            Y.append(data[idx]['res'])
-    
+            Y.append(data[idx]["res"])
+
     if get_pseudo_label:
         return X, Z, Y
     else:
         return X, None, Y
 
-def get_hwf(train = True, get_pseudo_label = False):
-    if(train):
-        file = './datasets/data/expr_train.json'
-    else:
-        file = './datasets/data/expr_test.json'
-    
-    return get_data(file, get_pseudo_label)
 
-if __name__ == "__main__":
-    train_X, train_Y = get_hwf(train = True)
-    test_X, test_Y = get_hwf(train = False)
-    print(len(train_X), len(test_X))
-    print(len(train_X[0]), train_X[0][0].shape, train_Y[0])
-    
+def get_hwf(train=True, get_gt_pseudo_label=False):
+    if train:
+        file = os.path.join(CURRENT_DIR, "data/expr_train.json")
+    else:
+        file = os.path.join(CURRENT_DIR, "data/expr_test.json")
+
+    return get_data(file, get_gt_pseudo_label)
